91d4b7791cc3c75370068967ac3755a74814ae6b
kouki-matsuura <kouki19991222@icloud.com>
2021-11-17 14:05:44 +0900

    [change] SECRET_KEY 変更

--- dataEdit/dataEdit/settings.py
+++ dataEdit/dataEdit/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+try:
+    from.local_settings import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5d+eemhg)2))oj5$wpgf=vu70!sh3*w_s#*d!8)=-cdr_*55$2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
